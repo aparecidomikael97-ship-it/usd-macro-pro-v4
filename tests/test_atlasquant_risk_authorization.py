184dabda3419fc9c4b16e00863130326694b7333
aparecidomikael97-ship-it <aparecidomikael97@gmail.com>
2026-09-23 05:54:14 -0400

    test(p0): make risk authorization fixtures execution-grade

--- tests/test_atlasquant_risk_authorization.py
+++ tests/test_atlasquant_risk_authorization.py
@@ -6,7 +6,7 @@ L=RiskLimits(1000,30,10,5,2,20,2); S=RiskState()
 def req(**kw):
  d=dict(opportunity_id="opp-1",pair="EUR/USD",strategy_version="AMD-1.0",requested_trade_risk=5,
  requested_exposure=5,structural_stop_valid=True,rr_after_costs=2,spread_ok=True,slippage_ok=True,
- liquidity_ok=True,volatility_ok=True,news_clear=True,data_fresh=True,opportunity_state="TRIGGERED")
+ liquidity_ok=True,volatility_ok=True,news_clear=True,data_fresh=True,opportunity_state="TRIGGERED",direction="SELL",entry_price=1.1,stop_price=1.2)
  d.update(kw); return RiskRequest(**d)
 
 def test_clean_request_gets_expiring_auth_but_no_real_orders():
